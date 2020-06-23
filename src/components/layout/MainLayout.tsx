e35b35699c36af180cc5003c22658d878efec89f
Diego Rayo <diegofrayo@gmail.com>
2020-06-22 20:56:29 -0500

    :sparkles: [create] modal component

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import Link from 'next/link';
 
+import { Modal } from '~/components/primitive';
+import twcss from '~/lib/twcss';
+
 export default function MainLayout({ children }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  function handleAddBussinessClick() {
+    setIsModalVisible(true);
+  }
+
   return (
     <main className="tw-mx-auto tw-max-w-screen-md tw-text-center tw-p-6">
       <header>
@@ -26,8 +36,13 @@ export default function MainLayout({ children }) {
                 <a className="tw-block tw-p-2">categorías</a>
               </Link>
             </li>
-            <li className="tw-flex-1 tw-text-yellow-400 tw-bg-gray-900 hover:tw-bg-gray-800 tw-font-bold tw-cursor-pointer tw-my-1 sm:tw-my-0 tw-p-2 tw-underline">
-              agrega un negocio!
+            <li className="tw-flex-1 tw-text-yellow-400 tw-bg-gray-900 hover:tw-bg-gray-800 tw-font-bold tw-cursor-pointer tw-my-1 sm:tw-my-0 tw-underline">
+              <button
+                className="tw-w-full tw-block tw-h-full tw-p-2"
+                onClick={handleAddBussinessClick}
+              >
+                agrega un negocio!
+              </button>
             </li>
           </ul>
         </nav>
@@ -49,6 +64,109 @@ export default function MainLayout({ children }) {
           </li>
         </ul>
       </footer>
+      <Modal
+        visible={isModalVisible}
+        onCloseHandler={() => {
+          setIsModalVisible(false);
+        }}
+      >
+        <section className="modal-content tw-bg-white tw-p-6 tw-relative tw-max-w-screen-md tw-w-screen-md tw-max-h-full tw-overflow-auto">
+          <section className="tw-px-6 tw-pt-6">
+            <Modal.CloseButton />
+            <h2 className="tw-font-bold tw-text-3xl">
+              agrega un negocio a este sitio web
+            </h2>
+          </section>
+
+          <section className="tw-my-6">
+            <label htmlFor="name" className="tw-text-left tw-block">
+              <Label>nombre</Label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="tw-w-full tw-border tw-border-black tw-border-2 tw-p-2"
+              />
+            </label>
+            <div className="tw-my-6" />
+
+            <label htmlFor="whatsapp" className="tw-text-left tw-block">
+              <Label>whatsapp</Label>
+              <div className="tw-w-full tw-flex tw-border tw-border-black">
+                <span className="tw-bg-gray-400 tw-text-gray-800 tw-flex tw-items-center tw-px-2 tw-text-sm tw-font-bold tw-border-r tw-border-black tw-w-12 tw-justify-center">
+                  +57
+                </span>
+                <input
+                  type="text"
+                  id="whatsapp"
+                  name="whatsapp"
+                  className="whatsapp tw-flex-1 tw-p-2"
+                />
+              </div>
+            </label>
+            <div className="tw-my-6" />
+
+            <label htmlFor="instagram" className="tw-text-left tw-block">
+              <Label>instagram</Label>
+              <div className="tw-w-full tw-flex tw-border tw-border-black">
+                <span className="tw-bg-gray-400 tw-text-gray-800 tw-flex tw-items-center tw-px-2 tw-text-sm tw-font-bold tw-border-r tw-border-black tw-w-12 tw-justify-center">
+                  @
+                </span>
+                <input
+                  type="text"
+                  id="instagram"
+                  name="instagram"
+                  className="instagram tw-flex-1 tw-p-2"
+                />
+              </div>
+            </label>
+            <div className="tw-my-6" />
+
+            <label htmlFor="facebook" className="tw-text-left tw-block">
+              <Label>facebook</Label>
+              <div className="tw-w-full tw-flex tw-border tw-border-black">
+                <span className="tw-bg-gray-400 tw-text-gray-800 tw-flex tw-items-center tw-px-2 tw-text-sm tw-font-bold tw-border-r tw-border-black tw-w-12 tw-justify-center">
+                  @
+                </span>
+                <input
+                  type="text"
+                  id="facebook"
+                  name="facebook"
+                  className="facebook tw-flex-1 tw-p-2"
+                />
+              </div>
+            </label>
+            <div className="tw-my-6" />
+
+            <label htmlFor="description" className="tw-text-left tw-block">
+              <Label>descripción</Label>
+              <textarea
+                id="description"
+                name="description"
+                className="description tw-w-full tw-border tw-border-black tw-border-2 tw-p-2"
+              />
+            </label>
+          </section>
+
+          <button className="tw-bg-black tw-text-white tw-py-2 tw-px-4">
+            agregar negocio
+          </button>
+        </section>
+      </Modal>
+
+      <style jsx>{`
+        .modal-content {
+          width: 600px;
+        }
+
+        .description {
+          resize: none;
+        }
+      `}</style>
     </main>
   );
 }
+
+// --- Components ---
+
+const Label = twcss.p`tw-mb-1 tw-font-bold tw-cursor-pointer`;
---
+++ src/components/primitive/Modal.tsx
@@ -0,0 +1,90 @@
+import { useRef, createContext, useContext } from 'react';
+
+function Modal({ children, visible, onCloseHandler }) {
+  const closeModal = () => {
+    onCloseHandler(false);
+  };
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <Context.Provider value={{ onCloseModalHandler: closeModal }}>
+      <Backdrop closeModalHandler={closeModal}>{children}</Backdrop>
+    </Context.Provider>
+  );
+}
+
+// --- Context ---
+
+const Context = createContext({ onCloseModalHandler: () => {} });
+
+Modal.Context = Context;
+
+// --- Components ---
+
+function Backdrop({ children, closeModalHandler }) {
+  const backdropRef = useRef(null);
+
+  const handleBackdropClick = e => {
+    if (backdropRef && backdropRef.current === e.target) {
+      closeModalHandler();
+    }
+  };
+
+  return (
+    <div
+      className="root tw-p-2 sm:tw-p-4"
+      ref={backdropRef}
+      onClick={handleBackdropClick}
+    >
+      {children}
+
+      <style jsx>
+        {`
+          .root {
+            align-items: center;
+            background-color: rgba(0, 0, 0, 0.5);
+            bottom: 0;
+            display: flex;
+            justify-content: center;
+            left: 0;
+            overflow: auto;
+            position: fixed;
+            right: 0;
+            top: 0;
+            z-index: 1000;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
+
+Modal.Backdrop = Backdrop;
+
+function CloseButton() {
+  const { onCloseModalHandler } = useContext(Context);
+
+  return (
+    <button className="close-button" onClick={onCloseModalHandler}>
+      <span className="tw-font-bold tw-text-3xl">X</span>
+
+      <style jsx>{`
+        .close-button {
+          height: auto;
+          position: absolute;
+          right: 20px;
+          text-shadow: 2px 2px 2px #d3d3d3;
+          top: 10px;
+          z-index: 30;
+        }
+      `}</style>
+    </button>
+  );
+}
+
+Modal.CloseButton = CloseButton;
+
+export default Modal;
