40554eba06bece8f70cd9b8c341d7eb89d172dbe
Diego Rayo <diegofrayo@gmail.com>
2020-07-30 11:58:13 -0500

    :bug: [fix] analytics bug

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -9,7 +9,7 @@ const ANALYTICS_TRACKING_ID = "UA-98284306-2";
 export function initAnalytics(): void {
   const isAnalyticsDisabled =
     window.location.href.includes("noga=true") ||
-    window.localStorage.getItem("noga") ||
+    window.localStorage.getItem("noga") === "true" ||
     isDevelopmentEnvironment("ANALYTICS");
 
   ReactGA.initialize(ANALYTICS_TRACKING_ID, {
